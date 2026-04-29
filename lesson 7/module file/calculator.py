33522c641448720e6d25f8dcee6ba7a5509f1f21
Krunal Retiwala <robostashub@gmail.com>
2026-04-29 12:44:38 +0530

    Just Uploaded !

---
+++ lesson 7/module file/calculator.py
@@ -0,0 +1,24 @@
+# module : a simple python file(.py) which includes
+# some functionalty like constant, functions, classes.
+# this script is intended to be imported in onether python file,
+# and then that functinalty are gling to be used !  
+
+
+def addition (a,b):
+    print(f"Addition is :{a+b}")
+
+def Subtraction (a,b):
+    print(f"Subtraction is :{a-b}")
+
+def multiplication (a,b):
+    print(f"multiplication is :{a*b}")
+
+def Division (a,b):
+    print(f"Division is :{a/b}")
+
+
+#print("hello world")
+#print(__name__)  
+
+#if __name__ == "__main__":
+#    addition(2,7)
---
+++ lesson 7/module file/mere project.py
@@ -0,0 +1,38 @@
+#import calculator
+#calculator.addition(2,5)
+
+
+#import calculator as calc
+#calc.addition(3,5)
+#calc.Subtraction(6,4)
+
+
+
+#from calculator import addition
+#from calculator import Subtraction
+#from calculator import addition,Subtraction
+#from calculator import addition as add, Subtraction as sub
+
+#add(3,5)
+#sub(6,4)
+
+
+# __name__with__main__
+
+'''
+- dunder name (__name__)
+- dunder main (__main__)
+'''
+# __name__ this method id globally available in python
+# which return following possible values : 1.__main__   2.module_name
+
+# when we print the value of __name__ by running a current file,
+# it privides __main__ as an output ! 
+
+# when we print the value of __name__ by running a Diffrent file,
+# where a module is imported, it privides module_name as an output !    
+
+# print(__name__) 
+
+
+
